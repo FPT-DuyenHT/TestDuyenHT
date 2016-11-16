8d20f5169b8883c5ee56637aee4e1e20b83b81bb
hanhi757 <hanhi757ptit@gmail.com>
2016-11-17 01:11:37 +0700

    Update MainTest2.java

--- MainTest2.java
+++ MainTest2.java
@@ -4,5 +4,11 @@ public class MainTest2 {
 	public static void main(String[] args) {
 		System.out.println("this is main2");
 		System.out.println("this is main3");
+		
+		System.out.println("this is main1");
+		System.out.println("this is main1");
+		
+		System.out.println("this is main1");
+		System.out.println("this is main1");
 	}
 }
