c60c1bc502baefaff471bcccb265620e7b3295e8
hanhi757 <hanhi757ptit@gmail.com>
2016-11-17 02:21:00 +0700

    commit Mcopy.java, MainTest2.java

---
+++ Mcopy.java
@@ -0,0 +1,11 @@
+package com.test;
+
+public class MainTest2 {
+	public static void main(String[] args) {
+		System.out.println("this is main1");
+		System.out.println("this is main6");
+		System.out.println("this is main7");
+		System.out.println("this is main8");
+		System.out.println("this is main9");
+	}
+}
